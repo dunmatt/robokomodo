ffb4c36151dc8223ed69dcc4c06391d0bb6f614b
M@ <mattdunlap+github@gmail.com>
2016-02-09 20:53:00 -0500

    added battery checks to the bootup proceedure

--- src/main/scala/com/github/dunmatt/robokomodo/Robot.scala
+++ src/main/scala/com/github/dunmatt/robokomodo/Robot.scala
@@ -2,6 +2,10 @@ package com.github.dunmatt.robokomodo
 
 import com.github.dunmatt.roboclaw._
 import org.slf4j.LoggerFactory
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Success
+import squants.electro.ElectricPotentialConversions._
 import squants.motion.{ AngularVelocity, Velocity }
 import squants.space.AngleConversions._
 import squants.space.LengthConversions._
@@ -16,12 +20,14 @@ case object SHUT_DOWN extends State
 // case object SELF_TEST extends State
 
 class Robot(serialPorts: Map[Byte, SerialPortManager]) {
+  import Robot._
   protected val log = LoggerFactory.getLogger(getClass)
   // TODO: clean up the mapping from motor controller channel to motor, as is there are redundancies
   val motors = RoboTriple( new Motor(0x86.toByte, true, -60 degrees)
                          , new Motor(0x86.toByte, false, 60 degrees)
                          , new Motor(0x87.toByte, false, 60 degrees))
   val radius = 146 millimeters  // this value from CAD, make sure it's up to date
+  val batteryCellCount = 4
   // TODO: add the weapon motor
   val requiredControllers = Set( motors.left.controllerAddress
                                , motors.right.controllerAddress
@@ -52,7 +58,7 @@ class Robot(serialPorts: Map[Byte, SerialPortManager]) {
   //   case _ => None   // TODO: log a warning
   // }
 
-  def aiLoop(): Unit = {
+  def aiLoop: Unit = {
     while (fsm.state != SHUT_DOWN) {
       aiStep
       // TODO: maybe sleep?
@@ -60,22 +66,50 @@ class Robot(serialPorts: Map[Byte, SerialPortManager]) {
     log.info("Shutting down.")
   }
 
-  def aiStep(): Unit = fsm.state match {
-    case NOT_STARTED => Thread.sleep(10)  // TODO: read the settings from the robot and check health
+  def aiStep: Unit = fsm.state match {
+    case NOT_STARTED => start
     case READY => Thread.sleep(10)
     case GoingTo(loc) => Unit  // TODO: write me
     case RUNNING => Unit  // TODO: write me
     case SHUT_DOWN => Unit
   }
 
-  def stop(): Unit = {
+  def start: Unit = {
+    // TODO: do something with the results...
+    serialPorts.keySet.map(startMotorController)
+  }
+
+  def startMotorController(addr: Byte): Future[Boolean] = {
+    val port = serialPorts(addr)
+    val version = port.sendCommand(ReadFirmwareVersion(addr))
+    version.foreach{ v => log.info(s"Found roboclaw at $addr ($v).") }
+    val voltageOkay = port.sendCommand(ReadMainBatteryVoltageSettings(addr)).map{ range =>
+      val expectedMin = MIN_LIPO_CELL_VOLTAGE * batteryCellCount
+      val expectedMax = MAX_LIPO_CELL_VOLTAGE * batteryCellCount
+      if (range.min == expectedMin && range.max == expectedMax) {
+        range
+      } else {
+        log.debug(s"Got unexpected battery voltage range $range, resetting.")
+        port.sendCommand(SetMainBatteryVoltages(addr, expectedMin, expectedMax))
+        Range(expectedMin, expectedMax)
+      }
+    }.flatMap{ range =>
+      port.sendCommand(ReadMainBatteryVoltage(addr)).andThen{ case Success(v) =>
+        val batteryLevel = (v - range.min) / (range.max - range.min) * 100
+        log.info(s"Main battery at $batteryLevel% ($v)")
+      }.map(range.contains)
+    }
+    voltageOkay
+  }
+
+  def stop: Unit = {
     fsm.state = READY
     serialPorts.foreach { case (addr, port) =>
       port.sendCommand(DriveM1M2WithSignedDutyCycle(addr, TwoMotorData(0d, 0d)))
     }
   }
 
-  def stopAndShutDown(): Unit = {
+  def stopAndShutDown: Unit = {
     stop
     fsm.state = SHUT_DOWN
   }
@@ -99,6 +133,11 @@ class Robot(serialPorts: Map[Byte, SerialPortManager]) {
     val b = DriveM2WithSignedSpeed(motors.rear.controllerAddress, freqs.rear)
     Set(a, b)
   }
+}
+
+object Robot {
+  val MIN_LIPO_CELL_VOLTAGE = 3.2 volts
+  val MAX_LIPO_CELL_VOLTAGE = 4.2 volts
 }
 
 case class RoboTriple[A](left: A, right: A, rear: A) {
